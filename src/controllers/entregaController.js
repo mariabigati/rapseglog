a572a98dea7a8e4bdd896065448bb3a40958a12e
mariabigati <mariasozua345@gmail.com>
2025-12-04 10:50:27 -0300

    Alterando funcionalidades de entrega e adicionando funcionalidade de update de status da entrega

--- src/controllers/entregaController.js
+++ src/controllers/entregaController.js
@@ -16,23 +16,52 @@ const entregaController = {
 
     cadastraEntregas: async (req, res) => {
         try {
-            const {id_pedido, id_status_entrega, valor_distancia, valor_peso, valor_base, acresimo, desconto, taxa_extra, valor_final} = req.body;
+            const {id_pedido, id_status_entrega} = req.body;
 
-            if (!id_pedido ||  !id_status_entrega || !valor_distancia || !valor_peso || !valor_base || !valor_final) {
+            if (!id_pedido ||  !id_status_entrega) {
                 return res.status(400).json({ message: 'Há dados faltantes! Tente novamente.'});
             }
 
-            if (isNaN(id_pedido) || isNaN(id_status_entrega) || isNaN(valor_distancia) || isNaN(valor_peso) || isNaN(valor_base) || isNaN(valor_final)) {
+            if (isNaN(id_pedido) || isNaN(id_status_entrega)) {
                 return res.status(400).json({ message: 'Há dados inválidos! Tente novamente.'});
             }
 
-            const resultado = await entregaModel.insertEntrega(id_pedido, id_status_entrega, valor_distancia, valor_peso, valor_base, acresimo, desconto, taxa_extra, valor_final);
+            const resultado = await entregaModel.insertEntrega(id_pedido, id_status_entrega);
 
             // if (resultado.affectedRows() === 0) {
             //     return res.status(200).json({ message: 'Houve um erro ao inserir os dados da entrega no sistema. Tente novamente!' });
             // }
 
             res.status(201).json({ message: 'Dados da entrega inseridos com sucesso!', data: resultado });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Ocorreu um erro no servidor.', errorMessage: error.message });
+        }
+    },
+
+    atualizaEstadoEntrega: async (req, res) => {
+        try {
+            const {id_pedido, status_entrega} = req.body;
+            let id_status_entrega;
+            if (!id_pedido ||  !status_entrega) {
+                return res.status(400).json({ message: 'Há dados faltantes! Tente novamente.'});
+            }
+
+            if (isNaN(id_pedido) || isNaN(status_entrega)) {
+                return res.status(400).json({ message: 'Há dados inválidos! Tente novamente.'});
+            }
+
+            if (status_entrega.toLowerCase() === "calculado"){
+                id_status_entrega = 1;
+            } else if {
+                
+            }
+                
+            const resultado = await entregaModel.insertEntrega(id_pedido, id_status_entrega);
+
+            res.status(201).json({ message: 'Dados da entrega inseridos com sucesso!', data: resultado });
+
 
         } catch (error) {
             console.error(error);
--- src/models/entregaModel.js
+++ src/models/entregaModel.js
@@ -8,12 +8,19 @@ const entregaModel = {
         return rows;
     },
 
-    insertEntrega: async(pIdPedido, pIdStatus, pValorDistancia, pValorPeso, pValorBase, pAcresimo, pDesconto, pTaxaExtra, pValorFinal) => {
-        const procedure = 'CALL cadastrar_nova_entrega(?, ?, ?, ?, ?, ?, ?, ?, ?);';
-        const values = [pIdPedido, pIdStatus, pValorDistancia, pValorPeso, pValorBase, pAcresimo, pDesconto, pTaxaExtra, pValorFinal];
+    insertEntrega: async(pIdPedido, pIdStatus) => {
+        const procedure = 'CALL cadastrar_nova_entrega(?, ?);';
+        const values = [pIdPedido, pIdStatus];
         const [rows] = await pool.query(procedure, values);
         return rows[0];
-    }
+    },
+
+    updateEstadoEntrega: async(pIdEntrega, pIdStatus) => {
+        const procedure = 'CALL update_estado_entrega(?, ?);';
+        const values = [pIdEntrega, pIdStatus];
+        const [rows] = await pool.query(procedure, values);
+        return rows[0];
+    },
 }
 
 module.exports = { entregaModel };
